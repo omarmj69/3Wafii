17378c6545f6b3d23f706601ed544b8b1dbc6fa6
omar aljazairi <omar-aljazairi@hotmail.com>
2021-06-15 21:35:19 +0200

    add filter

--- 3Wafii/App_Start/FilterConfig.cs
+++ 3Wafii/App_Start/FilterConfig.cs
@@ -9,6 +9,7 @@ namespace _3Wafii
         {
             filters.Add(new HandleErrorAttribute());
             //filters.Add(new AuthorizeAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
